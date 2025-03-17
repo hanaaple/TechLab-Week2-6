d55497941020e56364be53e7be5aff0e1c5f716e
UISOO <seohsin1@naver.com>
2025-03-17 13:29:26 +0900

    Develop Resize Window (#15)

--- Source/Core/Rendering/UI.h
+++ Source/Core/Rendering/UI.h
@@ -6,8 +6,6 @@
 
 #include "URenderer.h"
 
-//class AActor;
-
 class USceneComponent;
 
 class UI
@@ -33,57 +31,16 @@ public: // UIWindows
     void RenderSettingsPanel();
 
 private:
-    // Mouse 전용
-    ImVec2 ResizeToScreenByCurrentRatio(const ImVec2& vec2) const
-    {
-        return {vec2.x / CurRatio.x, vec2.y / CurRatio.y};
-    }
-
-    ImVec2 ResizeToScreen(const ImVec2& vec2) const
-    {
-        float ratio = GetMin();
-        float preMin = GetPreMin();
-        return {vec2.x * PreRatio.x / CurRatio.x * ratio / preMin, vec2.y * PreRatio.y / CurRatio.y * ratio / preMin};
-    }
-
-    ImVec2 GetRatio() const
-    {
-        return {ScreenSize.x / InitialScreenSize.x, ScreenSize.y / InitialScreenSize.y};
-    }
-
-    float GetMin() const
-    {
-        if (CurRatio.x < CurRatio.y)
-        {
-            return CurRatio.x;
-        }
-        else
-        {
-            return CurRatio.y;
-        }
-    }
-
-    float GetPreMin() const
-    {
-        if (PreRatio.x < PreRatio.y)
-        {
-            return PreRatio.x;
-        }
-        else
-        {
-            return PreRatio.y;
-        }
-    }
 
     void RenderComponentTree(USceneComponent* Component, bool bShowTransform,
                              bool bShowUUID, ImGuiTreeNodeFlags nodeFlags);
 
 
-    bool bWasWindowSizeUpdated = true;
+    //bool bWasWindowSizeUpdated = true;
 
     ImVec2 ScreenSize;
-    ImVec2 InitialScreenSize;
+    //ImVec2 InitialScreenSize;
 
-    ImVec2 PreRatio;
-    ImVec2 CurRatio;
+    //ImVec2 PreRatio;
+    //ImVec2 CurRatio;
 };
